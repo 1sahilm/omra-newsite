b27ae6026a0791c07c52dfe3785dcf67fec45b56
SahilMishraDev <msahil76640@gmail.com>
2022-09-29 20:51:23 +0530

    changes

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -3,4 +3,5 @@ module.exports = {
   siteUrl: process.env.SITE_URL || "https://www.omrasolutions.com/",
   generateRobotsTxt: true, // (optional)
   // ...other options
+  sitemapBaseFileName: "sitemap",
 };
