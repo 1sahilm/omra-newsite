e2332fb2c767f3c7a1e9a661903ac31fa41c8f7e
SahilMishraDev <msahil76640@gmail.com>
2022-10-01 09:50:17 +0530

    changes

--- next.config.js
+++ next.config.js
@@ -1,12 +1,26 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
+module.exports = {
   eslint: {
     // Warning: This allows production builds to successfully complete even if
     // your project has ESLint errors.
     ignoreDuringBuilds: false,
   },
-};
 
-module.exports = nextConfig;
+  async rewrites() {
+    return [
+      {
+        source: "/sitemap.xml",
+        destination: "/api/sitemap",
+      },
+    ];
+  },
+  webpack: (config, { isServer }) => {
+    // Fixes npm packages that depend on `fs` module
+    if (!isServer) {
+      config.node = {
+        fs: "empty",
+      };
+    }
+
+    return config;
+  },
+};
