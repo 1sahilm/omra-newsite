9188b1103e92d8af88f66e22bcc2d6f43ed53c4b
SahilMishraDev <msahil76640@gmail.com>
2022-10-01 09:46:37 +0530

    changes

--- next.config.js
+++ next.config.js
@@ -4,23 +4,4 @@ module.exports = {
     // your project has ESLint errors.
     ignoreDuringBuilds: false,
   },
-
-  async rewrites() {
-    return [
-      {
-        source: "/sitemap.xml",
-        destination: "/api/sitemap",
-      },
-    ];
-  },
-  webpack: (config, { isServer }) => {
-    // Fixes npm packages that depend on `fs` module
-    if (!isServer) {
-      config.node = {
-        fs: "empty",
-      };
-    }
-
-    return config;
-  },
 };
