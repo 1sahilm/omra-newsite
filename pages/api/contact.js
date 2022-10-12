2e5becd181f621f058ff748a6a36f81542f72012
SahilMishraDev <msahil76640@gmail.com>
2022-10-12 14:15:15 +0530

    changes in contact page

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -24,7 +24,7 @@ export default async (req, res) => {
       bcc: "sahil.mishra@epicglobal.co.in",
       bcc: "omra.digitalsolution@gmail.com",
 
-      subject: `Contact form submission from ${name}`,
+      subject: `HRMS ${name}`,
       html: `<p>You have a contact form submission</p>
       <p><strong>Name: </strong> ${name}</p>
         <p><strong>Email: </strong> ${email}</p>
